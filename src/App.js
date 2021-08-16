63e0d477b23fcdb85434cc6c678ff7fbe7bf778b
Mykhailo Konontsev <fater181@gmail.com>
2021-08-16 16:06:53 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>
+          Hello From Menherachan AIB
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
